811f9467d36f01b1e6e63a5de0193e6fc852d504
Aaron Cox <aaron.cox@greymass.com>
2012-08-22 19:44:51 -0500

    Added config.php for tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,10 @@
 <?php
-require(dirname(__FILE__) . '/../Mongo.php');
+$testFolder = dirname(__FILE__);
+require($testFolder . '/../Mongo.php');
+if(file_exists($testFolder . '/config.php')) {
+	require($testFolder . '/config.php');
+}
+if(!defined('TEST_CONNECTION_STRING')) {
+	define('TEST_CONNECTION_STRING', '127.0.0.1');
+}
+Epic_Mongo::addConnection('default', TEST_CONNECTION_STRING);
