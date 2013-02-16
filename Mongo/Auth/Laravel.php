01cf0c07ac79153e8d6710fef297429bcf81efac
Aaron Cox <aaron.cox@greymass.com>
2013-02-15 18:11:18 -0600

    Added alt username compatibility

--- Mongo/Auth/Laravel.php
+++ Mongo/Auth/Laravel.php
@@ -33,19 +33,34 @@ class Epic_Mongo_Auth_Laravel extends \Laravel\Auth\Drivers\Driver {
 		if(!Config::has('auth.username')) {
 			throw new Exception('The username in application/config/auth.php must be defined.');
 		}
+		
 		$model = Config::get('auth.model');
+		// Add the username to the query
 		$query = array(
-			Config::get('auth.username') => $arguments[Config::get('auth.username')]
+			'$or' => array(
+				array(
+					Config::get('auth.username') => $arguments[Config::get('auth.username')]
+				)
+			), 
 		);
+		// If we've specified an 'username_alt' field in the config, add that to the $OR
+		if(Config::has('auth.username_alt')) {
+			$query['$or'][] = array(
+				Config::get('auth.username_alt') => $arguments[Config::get('auth.username')]
+			);
+		}
 		$user = Epic_Mongo::db('user')->findOne($query);
 		// This driver uses a basic username and password authentication scheme
 		// so if the credentials match what is in the database we will just
 		// log the user into the application and remember them if asked.
 		$password = $arguments[Config::get('auth.password')];
 		// if ( ! is_null($user) and Hash::check($password, $user->password))
-		if ( ! is_null($user) and Hash::check($password, $user->password))
-		{
+		if (!is_null($user) and Hash::check($password, $user->password)) {
 			return $this->login($user->_id, array_get($arguments, 'remember'));
+		} else {
+			if(!is_null($user) and md5($password) == $user->password) {
+				return $this->login($user->_id, array_get($arguments, 'remember'));				
+			}
 		}
 
 		return false;
