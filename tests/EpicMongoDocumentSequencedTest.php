571514681cb8b0019ed56e10f8ee041c24c8dceb
Aaron Cox <aaron.cox@greymass.com>
2013-04-05 21:04:27 -0500

    Added single test that the class exists.

--- tests/EpicMongoDocumentSequencedTest.php
+++ tests/EpicMongoDocumentSequencedTest.php
@@ -1,5 +1,8 @@
 <?php
 
 class EpicMongoDocumentSequencedTest extends PHPUnit_Framework_TestCase {
-	
+	public function testEpicMongoDocumentExists()
+	{
+		$this->assertTrue(class_exists('Epic_Mongo_Document_Sequenced'));
+	}
 }
